a721c20a7b71357ca15200900d7713925c45421c
Nived Km <nivedkm588@gmail.com>
2024-02-01 11:18:26 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div>
+       <h1 className='text-2xl font-bold text-blue-400 underline'>Hello world</h1>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
